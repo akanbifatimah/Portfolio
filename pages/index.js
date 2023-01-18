8f87d88a150ec053bbbe1c8237eefb4880913c29
Fatimah Akanbi <akanbifatimah@gmail.com>
2023-01-18 17:46:06 +0100

    added SEO

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,16 @@ import Testimonial from "../components/UI/Testimonial";
 import Contact from "../components/UI/Contact";
 
 export default function Home() {
+  <Head>
+    <meta name="description" content="Front end web developer" />
+  <meta property="og:title" content="Home - Fatimah Akanbi" />
+  <meta property="og:description" content="All about Fatimah Akanbi-web developer!" />
+  <meta property="og:url" content="https://fatimahakanbi.vercel.app/" />
+  <meta property="og:type" content="website" />
+  <meta name="viewport" content="width=device-width,
+   initial-scale=1.0"/>
+
+  </Head>
   return (
     <Fragment>
       <Hero />
