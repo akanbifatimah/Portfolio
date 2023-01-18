eff0d42e58078dc3a38b0cfe70d297cc64415a3c
Fatimah Akanbi <akanbifatimah@gmail.com>
2023-01-18 18:44:35 +0100

    added title

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,10 @@ import Contact from "../components/UI/Contact";
 
 export default function Home() {
   <Head>
+  <title>Fatimah Akanbi</title>
     <meta name="description" content="Front end web developer" />
   <meta property="og:title" content="Home - Fatimah Akanbi" />
-  <meta property="og:description" content="All about Fatimah Akanbi-web developer!" />
+  <meta property="og:description" content="Fatimah Akanbi-Front end web developer!" />
   <meta property="og:url" content="https://fatimahakanbi.vercel.app/" />
   <meta property="og:type" content="website" />
   <meta name="viewport" content="width=device-width,
